fe31b1c1e6a9334af579beaf5d48e290470737bd
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-22 17:53:41 +0530

    19/06

---
+++ 41_math_case.py
@@ -0,0 +1,16 @@
+#write program to print state full name according to short name
+shortState= input("enter state short name : ")# 
+match shortState:
+    case "mp":
+        print("madhya-pradesh")
+    case "up":
+        print("utter - pradesh")  
+    case "rj":
+        print("rajsthan")
+    case "td":
+        print("tamilnadu") 
+    case _:
+        print("not valid name")
+
+
+
