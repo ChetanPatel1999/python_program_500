685e8c5c36198ba411409ad3c75469cafd955ed4
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-27 18:09:57 +0530

    07/07

---
+++ 164_string_methods.py
@@ -0,0 +1,46 @@
+# string methods 
+# s= "hello world institute"
+# print(s)
+# newstr= s.capitalize()
+# print(newstr)
+
+
+# s= "hello world institute"
+# print(s)
+# newstr= s.title()
+# print(newstr)
+
+
+# startwith()
+# s= input("enter your name : ")
+
+# if s.startswith("mr."):
+#     print("valid name")
+# else:
+#     print("invalid name please start with mr.")    
+
+#endswith()
+# s= input("enter your gmail  : ")
+
+# if s.endswith("@gmail.com"):
+#     print("valid gmail")
+# else:
+#     print("invalid gmail enter correct gmail")   
+# 
+
+#find :- return index of pass substring
+# s="indore city is clean city"
+
+# index= s.find("dp")
+
+# print(index)
+
+
+#index():- its also return index  but if sub string is not 
+#not find its given error
+# s="indore city is clean city"
+# index= s.index("city")
+# print(index)
+
+
+
---
+++ 166_join_method.py
@@ -0,0 +1,20 @@
+#join():-
+# s="ram is good boy"
+# newstr = "+".join(s)
+# print(s)
+# print(newstr)
+
+
+# sum=0
+# s=""
+# for i in range(1,9):
+#     sum=sum+i
+#     s=s+ str(i)
+
+# print("+".join(s) ,"=",sum)
+
+
+# count:- count a character present how many times.
+s="ram is agood boay"
+print(s.count('a'))
+
