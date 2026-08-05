4e77f684c80105c31ce65e6713c683e09ec651a2
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-08-05 18:00:50 +0530

    03/08

---
+++ 189_dict.py
@@ -0,0 +1,14 @@
+#how to add new value in dictionary 
+
+marks= {"hindi" : 80 , "english" : 90 , "math" :50}
+
+# its add new value in dictionary and also update old key value
+# marks["science"]=45
+
+
+# using update method we can add new element and also update old key value
+# marks.update({"science":20})
+marks.update({"science":20,"so-science":70})
+
+print(marks)
+
---
+++ 190_dict.py
@@ -0,0 +1,22 @@
+marks= {"hindi" : 80 , "english" : 90 , "math" :50,"science":20,"so-science":70}
+
+# v1= marks.values()
+
+# print(v1)
+
+# for value in v1:
+#     print(value)
+
+
+# for value in marks.values():
+#     print(value)
+
+keys= marks.keys()
+
+print(keys)
+
+# for key in keys:
+#     print(key)
+
+for key in marks.keys():
+    print(key)
